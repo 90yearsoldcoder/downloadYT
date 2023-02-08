20d0cc7365f07cbae97747dd7acb63159d5fb0e6
90yearsoldcoder <minty@bu.edu>
2023-02-07 23:19:44 -0500

    add download function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a mini program to download Youtube Video
-
+import pytube.request
 from pytube import YouTube
 from pytube.cli import on_progress
 import time
@@ -15,26 +15,28 @@ def typewrite(num1, num2, text):
 
 def completed(stream, file_path):
     comp = '\nDownload Completed\n'
-    size = 'File Size: ' + str(stream.filesize / 2 ** 30) + "Mb\n"
+    size = 'File Size: ' + str(stream.filesize / 2 ** 20) + "Mb\n"
     title = 'Title: ' + stream.title + '\n'
     Local = "Local Path: " + file_path + '\n'
 
     txt_list = [comp, size, title, Local]
 
     for txt in txt_list:
-        typewrite(0.05, 0.1, txt)
+        typewrite(0.01, 0.05, txt)
         print('-' * 60)
 
 
-def download(url: str, path: str):
+def download(url: str, path: str, cap_flag: bool):
     try:
         yt = YouTube(url, on_progress_callback=on_progress, on_complete_callback=completed)
-
         stream = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
-        typewrite(0.05, 0.1, "Download is starting\n")
+        typewrite(0.01, 0.05, "The Video is found\n")
+        typewrite(0.01, 0.05, "Start to download the Video...\n")
         stream.download(output_path=path)
+
     except:
         print('Something went wrong in function download')
+
 
 
 if __name__ == '__main__':
